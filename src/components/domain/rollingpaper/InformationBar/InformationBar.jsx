f6440cf8d0333607b407fe1811aed8d27282f06e
numi8462 <yhk8462@naver.com>
2025-03-07 13:56:40 +0900

    feat: Toast 기능 및 InformationBar 추가

---
+++ src/components/domain/rollingpaper/InformationBar/InformationBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import Emoji from '../../../common/Emoji/Emoji';
+import useToast from '../../../common/Toast/useToast';
+import Toast from '../../../common/Toast/Toast';
+
+const StyledInformationBar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 68px;
+`;
+
+const StyledName = styled.div`
+  font-size: 28px;
+  font-weight: 700;
+`;
+
+function InformationBar({ name, messageCount, emojis }) {
+  const { toast, showToast, closeToast } = useToast();
+
+  const handleClick = () => {
+    const currentUrl = window.location.href;
+    navigator.clipboard
+      .writeText(currentUrl)
+      .then(() => {
+        showToast('URL이 복사되었습니다.');
+      })
+      .catch((err) => {
+        console.error('URL 복사에 실패했습니다.', err);
+        showToast('URL 복사에 실패했습니다.');
+      });
+  };
+
+  return (
+    <StyledInformationBar>
+      <StyledName>To. {name}</StyledName>
+      <Emoji emoji={'\u{1F600}'} count={2} />
+      <button onClick={handleClick}>URL복사</button>
+      {toast && <Toast message={toast.message} onClose={closeToast} />}
+    </StyledInformationBar>
+  );
+}
+
+export default InformationBar;
