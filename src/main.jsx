62fade6ef8c27c0b4c2ac9aed098a4c04ffca9ec
김태욱 <idididsjkim@gmali.com>
2025-03-11 23:39:00 +0900

    feat: Rollingpaper.jsx 페이지 사이트 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home/HomePage.jsx';
-import RollingPaperList from './pages/RollingPaperList/RollingPaperList.page.jsx';
-import RollingPaper from './pages/RollingPaper/RollingPaper.page.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage.jsx";
+import RollingPaperList from "./pages/RollingPaperList/RollingPaperList.page.jsx";
+import RollingPaper from "./pages/RollingPaper/RollingPaper.page.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
           <Route path="list" element={<RollingPaperList />} />
-          <Route path="post">
+          <Route path="post" element={<RollingPaperMaking />}>
             <Route path=":id" element={<RollingPaper />} />
           </Route>
         </Route>
