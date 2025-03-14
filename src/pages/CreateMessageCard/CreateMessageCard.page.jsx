2b55026c0eed64dc1d2858ccc347678fd442bfb2
K0seoyoung <koseo12@hufs.ac.kr>
2025-03-14 17:28:49 +0900

    refactor: relationship

--- src/pages/CreateMessageCard/CreateMessageCard.page.jsx
+++ src/pages/CreateMessageCard/CreateMessageCard.page.jsx
@@ -50,7 +50,7 @@ const S = {
 export default function CreateMessageCard() {
     const [selectItem, setSelectItem] = useState('');
 
-    const realations = [
+    const relationship = [
         {value: '지인', label: '지인'},
         {value: '동료', label: '동료'},
         {value: '가족', label: '가족'},
@@ -85,8 +85,8 @@ export default function CreateMessageCard() {
                     <S.ContentWrapper>
                         <S.Label $bold>상대와의 관계</S.Label>
                         <Dropdown 
-                            placeholder={realations[0].label}
-                            options={realations}
+                            placeholder={relationship[0].label}
+                            options={relationship}
                             disabled={false}
                             onChange={handleSelectChange}
                             errorMessage="관계를 선택해주세요."
