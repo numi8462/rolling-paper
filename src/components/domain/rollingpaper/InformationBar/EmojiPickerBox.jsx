f67b073955d33d059c7398c0f31d08423df4518f
numi8462 <yhk8462@naver.com>
2025-03-18 23:21:14 +0900

    style: emojiPicker 위치 수정

--- src/components/domain/rollingpaper/InformationBar/EmojiPickerBox.jsx
+++ src/components/domain/rollingpaper/InformationBar/EmojiPickerBox.jsx
@@ -1,29 +1,32 @@
-import EmojiPicker from "emoji-picker-react";
-import useCreateReaction from "../../../common/hooks/reactions/useCreateReaction";
-import styled from "styled-components";
+import EmojiPicker from 'emoji-picker-react';
+import useCreateReaction from '../../../common/hooks/reactions/useCreateReaction';
+import styled from 'styled-components';
+import { media } from '../../../../styles/theme';
 
 const EmojiPickerWrap = styled.div`
   position: absolute;
-  top:42px; 
-  right:0;
+  top: 48px;
+  right: 0;
+  ${media.mobile`
+    right: -60px;
+  `}
 `;
 
-function EmojiPickerBox({postId, refetch, open, setOpen}) {
-  
+function EmojiPickerBox({ postId, refetch, open, setOpen }) {
   const { createReaction } = useCreateReaction(refetch);
-    const chosenEmoji = {
-      "emoji": null,
-      "type": "increase"
-    };
-  
-    const onEmojiClick = (emojiData) => {
-      createReaction(postId, {
-        emoji: emojiData.emoji,
-        type: chosenEmoji.type, // type은 그대로 유지
-      });
-      setOpen(false)
-    };
-  
+  const chosenEmoji = {
+    emoji: null,
+    type: 'increase',
+  };
+
+  const onEmojiClick = emojiData => {
+    createReaction(postId, {
+      emoji: emojiData.emoji,
+      type: chosenEmoji.type, // type은 그대로 유지
+    });
+    setOpen(false);
+  };
+
   return (
     <EmojiPickerWrap>
       <EmojiPicker open={open} onEmojiClick={onEmojiClick} />
