753a848dff38234054c2171d78f22b0addf8156a
numi8462 <yhk8462@naver.com>
2025-03-18 22:53:29 +0900

    style: navbar 767px 이하 숨김

--- src/components/common/Navbar/NavbarCore.jsx
+++ src/components/common/Navbar/NavbarCore.jsx
@@ -6,7 +6,7 @@ import { Container } from '../../../styles/theme';
 import { useEffect, useState } from 'react';
 
 const BREAK_POINT = {
-  mobile: 360, // 변경된 부분
+  mobile: 767,
   tablet: 1248,
 };
 
@@ -68,7 +68,7 @@ const LogoText = styled.span`
   line-height: 29.96px;
   text-align: center;
   margin-left: 8px;
-  color: #4A494F;
+  color: #4a494f;
 `;
 
 const ButtonContainer = styled.div`
@@ -144,37 +144,6 @@ export default function Navbar() {
   const hideOnMobile =
     window.innerWidth <= BREAK_POINT.mobile &&
     !['/', '/list'].includes(location.pathname);
-//   const [isMobile, setIsMobile] = useState(window.innerWidth <= BREAK_POINT.mobile);
-//   const [currentPath, setCurrentPath] = useState(location.pathname);
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setIsMobile(window.innerWidth <= BREAK_POINT.mobile);
-//     };
-
-//     const handleLocationChange = () => {
-//       setCurrentPath(location.pathname);
-//     };
-
-//     // 화면 크기 변경 시 감지
-//     window.addEventListener('resize', handleResize);
-
-//     // 페이지 경로 변경 시 감지
-//     window.addEventListener('popstate', handleLocationChange); // 페이지 뒤로가기, 앞으로가기
-
-//     // 초기 경로 설정
-//     setCurrentPath(location.pathname);
-
-//     return () => {
-//       window.removeEventListener('resize', handleResize);
-//       window.removeEventListener('popstate', handleLocationChange);
-//     };
-//   }, []);
-
-//   // 모바일 크기일 때 '/post' 경로가 포함되면 Navbar 숨기기
-//   if (isMobile && currentPath.includes('/post')) {
-//     return null;
-//   }
 
   return (
     <NavbarWrapper showOnMobile={showOnMobile} hideOnMobile={hideOnMobile}>
